d5995cc5d234a016d15fab801c584790b7ae9f9b
meinseo <91935025+meinseo@users.noreply.github.com>
2021-10-26 14:36:00 +0900

    Add files via upload

---
+++ doc/slick01.js
@@ -0,0 +1,19 @@
+$(function(){
+//------------------------------
+$('.main_slider').slick({
+    arrows:false, 
+    autoplay:true,
+    pauseOnHover:false,
+    //vertical:true,   //아래위로 움직이는것
+    //fade:true,       //흐려졌다 나온다 
+});
+
+
+
+
+
+
+
+
+//-----------------------------
+})
