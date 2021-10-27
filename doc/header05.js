ca9519432b4fafd9ecda8b58c694b74c928670cb
meinseo <91935025+meinseo@users.noreply.github.com>
2021-10-27 14:58:03 +0900

    Add files via upload

---
+++ doc/header05.js
@@ -0,0 +1,55 @@
+$(function(){
+//---------------------------------
+
+//스크롤할 때 헤드변경 addClass
+
+$(window).on('scroll',function(){
+    var sct=$(window).scrollTop();
+    console.log(sct);
+    if(sct>0) {
+        $('#header').addClass('on');
+    }else{
+        $('#header').removeClass('on');
+    }
+});
+    
+
+
+
+
+
+
+
+    //------------------------------
+$('.main_slider').slick({
+    arrows:false, 
+    autoplay:true,
+    pauseOnHover:false,
+
+});
+
+
+
+
+
+
+//제품 슬라이드 
+$('.pr_slider').slick({
+    arrows:false,
+    autoplay:true,
+    slidesToShow: 5,
+    dots:true,
+});
+
+
+$('#content01 .btn i.xi-arrow-left').on ('click',function(){
+    $('.pr_slider').slick('slickPrev');
+});
+$('#content01 .btn i.xi-arrow-right').on ('click',function(){
+    $('.pr_slider').slick('slickNext');
+});
+
+
+
+//-----------------------------
+})
--- doc/slick02.js
+++ doc/slick02.js
@@ -1,5 +1,7 @@
 $(function(){
 //------------------------------
+
+//메인슬라이드
 $('.main_slider').slick({
     arrows:false, 
     autoplay:true,
@@ -8,8 +10,32 @@ $('.main_slider').slick({
 });
 
 
+//메인슬라이드 버튼
+$('#main_visual span.left').on('click',function(){
+    $('.main_slider').slick('slickPrev');
+});
+$('#main_visual span.right').on('click',function(){
+    $('.main_slider').slick('slickNext');
+});
+
+//메인슬라이드 dot기능 넣기 
+$('#main_visual .list li').on('click',function(){
+    var idx=$(this).index();
+    console.log(idx);
+    $('.main_slider').slick('slickGoTo',idx);
+    $(this).addClass('on').siblings().removeClass('on');
+});
 
 
+//slick될떄 list bg 이동
+var z=1;
+$('#main_visual .list>li').eq(0).addClass('on');
+$('.main_slider').on('afterChange',function(e,s,c){
+    console.log(c);
+    $('#main_visual .list>li').eq(c).addClass('on').siblings().removeClass('on');
+    $('#main_visual .photo').css ({transform:'rotate('+120*z+'deg)'});
+    z++;
+})
 
 
 //제품 슬라이드 
@@ -19,6 +45,8 @@ $('.pr_slider').slick({
     slidesToShow: 5,
     dots:true,
 });
+
+
 
 
 $('#content01 .btn i.xi-arrow-left').on ('click',function(){
