30e07ed45f49589d4b9f0c28e2be217a49818c28
meinseo <91935025+meinseo@users.noreply.github.com>
2021-11-03 14:05:29 +0900

    Add files via upload

---
+++ doc/basic05.js
@@ -0,0 +1,25 @@
+$(function(){
+
+//-----------------------------------------------------------------
+$('#top_banner i').on('click', function(){
+   //$('#top_banner').hide();
+    $('#top_banner').slideUp();
+});
+
+$('.main_slider').slick({
+    arrows:false,
+    autoplay:true,
+    autoplaySpeed:2000,
+    pauseOnHover:false,
+    pauseOnFocus:false,
+});
+ 
+
+
+//function topBannerClose(){
+//   $('#top_banner').hide();
+//}
+
+//-----------------------------------------------------------------
+
+});
