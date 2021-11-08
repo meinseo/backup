82ca4b68bb84229334bdf19c776b50c874617828
meinseo <91935025+meinseo@users.noreply.github.com>
2021-11-08 14:02:59 +0900

    Add files via upload

---
+++ doc/responsive01.js
@@ -0,0 +1,25 @@
+$   (function(){
+$ ('#header i').on('click', function(){
+    $('nav').toggle();
+});
+
+$(window).on('resize', function(){
+    $('nav').removeAttr ('style');
+});
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+// toggle 보였다 안보였다//
