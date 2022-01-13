819ed91edaa3485a2c8c5a098a5ede16bde6c55f
meinseo <91935025+meinseo@users.noreply.github.com>
2022-01-13 15:34:32 +0900

    Add files via upload

---
+++ pf07/js/pf07.js
@@ -0,0 +1,33 @@
+$  (function(){
+//----------------------------------------
+
+
+// 메인비주얼 동영상
+$(".bgndVideo").YTPlayer({
+    videoURL:'https://www.youtube.com/embed/3mr43DHz2_0',
+    containment:'self',       
+    autoPlay:true, 
+    mute:true, 
+    startAt:0, 
+    opacity:1,
+    showControls:false,
+    optimizeDisplay:false,
+    ratio: '16/9'
+});
+
+
+
+    
+//----------------------------------------
+});
+    
+    
+{/* <iframe width="424" height="238" src="https://www.youtube.com/embed/C-BwGCtWHTM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+
+
+// 경희의 가을
+// <iframe width="950" height="564" src="https://www.youtube.com/embed/bZxUmJ8lLvI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+// 경희의 봄
+// <iframe width="950" height="534" src="https://www.youtube.com/embed/3mr43DHz2_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
