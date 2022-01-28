d3e08bac3501623179b7d918227f3652dd605cd7
meinseo <91935025+meinseo@users.noreply.github.com>
2022-01-28 19:31:37 +0900

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -5,21 +5,26 @@ $(function(){
     let sideBar=$('nav li');
     $('#main').fullpage({
         anchors:['intro', 'portfolio_01', 'portfolio_02', 'portfolio_03', 'portfolio_04', 'portfolio_05', 'profile'],
+        css3:false,
         afterLoad:function(origin, destination, direction){
             let idx=destination.index;
             sc.eq(idx).addClass('on').siblings().removeClass('on');
             sideBar.eq(idx).addClass('on').siblings().removeClass('on');
+            idx==0||idx==6 ? $('.cover_btn span').css({background:'#fff'}) :  $('.cover_btn span').css({background:'#f25329'}); 
         },
+
+        
     });
     
     
     //WEB is My MONEY
     new Typed('.slogan_txt',{
-        strings: ['WEB is My MONEY', 'gggg baby'],
-        typeSpeed:100,
+        strings: ['생각하는 디자이너,', '2022 <strong>meinseo</strong> PORTFOLIO !'],
+        typeSpeed:150, 
     });
     
     $('.cover_btn').on('click', function(){
+        $(this).toggleClass('on');
         $("#cover").slideToggle(); //fadeToggle
     });
     
