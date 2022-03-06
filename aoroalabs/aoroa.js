54fbf4578a45da1fd91f5f830849ac00d24c83ab
meinseo <91935025+meinseo@users.noreply.github.com>
2022-03-07 00:12:31 +0900

    Add files via upload

--- aoroalabs/aoroa.js
+++ aoroalabs/aoroa.js
@@ -2,7 +2,7 @@
 $(function(){
 
     $('#main').fullpage({
-    anchors:[ 'section01', 'section02', 'section03', 'section04' , 'section05' , 'section06'],
+    anchors:[ 'section01', 'section02', 'section03', 'section04' , 'section05' , 'footer'],
     css3:false,
     afterLoad: function(origin, destination, direction){
         var idx=destination.index;
